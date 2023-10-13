dab79a983bb78c5ad400606a3c1d46290555bf81
Daniel Pérez Arenas <144001482+Danielperezarenas@users.noreply.github.com>
2023-10-13 18:30:56 +0000

    Update send 2nd project

--- src/app.js
+++ src/app.js
@@ -1,37 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-import "./index.html";
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = () => {
-  //write your code here
-  document.querySelector("#newEx").addEventListener("click", () => {
-    document.querySelector("#excuse").innerHTML = generateExcuse();
-  });
-
-  console.log("Hello Rigo from the console! ");
-};
-
-let generateExcuse = () => {
   let who = ["My dog", "My cat", "My rabbit", "My bird"];
   let action = ["eat", "broke", "lost"];
   let thing = ["my homework", "my pants", "my food"];
   let when = ["yesterday", "today", "last night"];
+  //write your code here
+  document.querySelector("#newEx").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse(
+      who,
+      action,
+      thing,
+      when
+    );
+  });
+  console.log("Hello Rigo from the console! ");
+};
 
-  let whoIndx = Math.floor(Math.random() * who.length);
-  let actIndx = Math.floor(Math.random() * action.length);
-  let thinIndx = Math.floor(Math.random() * thing.length);
-  let whenIndx = Math.floor(Math.random() * when.length);
-
+let generateExcuse = (param1, param2, param3, param4) => {
+  let whoIndx = Math.floor(Math.random() * param1.length);
+  let actIndx = Math.floor(Math.random() * param2.length);
+  let thinIndx = Math.floor(Math.random() * param3.length);
+  let whenIndx = Math.floor(Math.random() * param4.length);
   return (
-    who[whoIndx] +
+    param1[whoIndx] +
     " " +
-    action[actIndx] +
+    param2[actIndx] +
     " " +
-    thing[thinIndx] +
+    param3[thinIndx] +
     " " +
-    when[whenIndx]
+    param4[whenIndx]
   );
 };
